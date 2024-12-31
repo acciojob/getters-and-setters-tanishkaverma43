2b5955ed924a012c09682e0a85b6148c15f68c4e
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-31 18:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+	this.name= name;
+	this.age= age;
+}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name, "is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(this.name, "is teaching")
+	}
+}
+
+let student  = new Student("Tanishka", 20);
+let teacher = new Teacher("krishna", 21);
+
+student.study();
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
