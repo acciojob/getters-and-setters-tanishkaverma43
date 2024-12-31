a62a313bd58e7ad68637153293d985d9a91cc774
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-31 19:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(this.name + "is studying")
+		console.log(this.name + " is studying")
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(this.name + "is teaching")
+		console.log(this.name + " is teaching")
 	}
 }
 
 let student  = new Student("Tanishka", 20);
-let teacher = new Teacher("krishna", 21);
+let teacher = new Teacher("Krishna", 21);
 
-student.study();
-teacher.teach();
+student.study(); // Output: Tanishka is studying
+teacher.teach(); // Output: Krishna is teachin
 
 // Do not change the code below this line
 window.Person = Person;
